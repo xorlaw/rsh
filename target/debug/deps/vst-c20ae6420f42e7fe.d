4091525784fb877cfc2c71a4eeb7dd900e8dc816
xorlawptop <xor@morke.org>
2026-03-05 21:52:24 +0000

    more gitignore

--- target/debug/deps/vst-c20ae6420f42e7fe.d
+++
@@ -1,5 +0,0 @@
-/home/law/coding/rust/vst/target/debug/deps/vst-c20ae6420f42e7fe.d: src/main.rs
-
-/home/law/coding/rust/vst/target/debug/deps/vst-c20ae6420f42e7fe: src/main.rs
-
-src/main.rs:
--- target/debug/vst.d
+++
@@ -1,1 +0,0 @@
-/home/law/coding/rust/vst/target/debug/vst: /home/law/coding/rust/vst/src/main.rs
